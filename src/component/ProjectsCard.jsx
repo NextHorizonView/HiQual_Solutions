5f71195df47124bb3fad2962013d2d7877a99701
Rajvardhan135 <rajvardhansinghchib@gmail.com>
2024-07-26 17:32:27 +0530

    project card fixed

--- src/component/ProjectsCard.jsx
+++ src/component/ProjectsCard.jsx
@@ -5,8 +5,8 @@ const ProjectsCard = ({ img, description, name }) => {
         <div className='flex flex-col items-center w-full bg-white rounded-2xl lg:min-w-[400px] lg:justify-end lg:rounded-[40px]'>
             <div className='flex flex-col h-full'>
                 <img className='object-cover h-full rounded-lg lg:rounded-[40px]' src={img} alt={name} />
-                <p className='p-4 text-lg font-medium text-left sm:text-xl md:text-2xl'>{name}</p>
-                <p className='text-sm p-4 text-[#5B5B5B] text-left sm:w-[70%] sm:text-base md:text-lg'>{description}</p>
+                <p className='px-6 mt-2 text-lg font-medium text-left sm:text-xl md:text-2xl'>{name}</p>
+                <p className='text-sm px-6 text-[#5B5B5B] text-left sm:w-[70%] sm:text-base md:text-lg'>{description}</p>
             </div>
         </div>
     )
