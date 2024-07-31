77532fc4abdc26f736d89a6b966c85cf2166e12e
Rajvardhan135 <rajvardhansinghchib@gmail.com>
2024-07-31 23:16:47 +0530

    final deployed code

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import ProjectDetail from "./component/ProjectDetail";
 import logo from './assets/horizon-view-logo.png'
 
 export default function App() {
-  // return <Routes>
-  //   <Route index element={<Home />} />
-  //   <Route path="/projects/:type/:id" element={<ProjectDetail />} />
-  // </Routes>
-  return <div className="w-screen h-screen bg-[#222222] p-8  flex flex-col justify-center items-center">
-    <img src={logo} />
-    <p className="text-lg text-center text-white">Need help? Contact us at <span className="text-[#495cf3]">nexthorizonview@gmail.com</span></p>
-  </div>
+  return <Routes>
+    <Route index element={<Home />} />
+    <Route path="/projects/:type/:id" element={<ProjectDetail />} />
+  </Routes>
 }
